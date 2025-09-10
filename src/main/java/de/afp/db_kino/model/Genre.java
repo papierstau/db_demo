fdd58e6d1816eb7c55614b4dd7c2ea1f15b48829
jflash <19jflash04@googlemail.com>
2025-09-10 13:15:53 +0200

    Add toString toGenre.java

--- src/main/java/de/afp/db_kino/model/Genre.java
+++ src/main/java/de/afp/db_kino/model/Genre.java
@@ -27,4 +27,9 @@ public class Genre {
         this.genre = genre;
     }
     
+    @Override
+    public String toString() {
+        return "GenreId: " + this.getGenreId() + " Genre: " + this.getGenre();
+    }
+    
 }
